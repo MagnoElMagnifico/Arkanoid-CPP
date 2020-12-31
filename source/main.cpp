2aeebffacd1817ab3eb956bb8052d291cb461a28
MagnoGG <marcosgranja317@gmail.com>
2020-12-31 20:19:11 +0100

    Improve the paddle movement and add the ball object

--- source/main.cpp
+++ source/main.cpp
@@ -13,15 +13,20 @@
 
 using namespace sf;
 
+const float speed = 10;
+
 int main()
 {
-    RenderWindow window(VideoMode(800, 600), "Arkanoid");
-    
+    RenderWindow window(VideoMode(600, 600), "Arkanoid");
+
     RectangleShape paddle(Vector2f(50, 10));
     paddle.setPosition(window.getSize().x / 2, window.getSize().y - 2 * paddle.getSize().y);
     paddle.setFillColor(Color::Black);
-    const float speed = 5;
-    
+
+    RectangleShape ball(Vector2f(10, 10));
+    ball.setPosition(window.getSize().x / 2, window.getSize().y / 2);
+    ball.setFillColor(Color::Black);
+
     while (window.isOpen())
     {
         Event event;
@@ -30,14 +35,26 @@ int main()
             if (event.type == Event::Closed)
                 window.close();
             if (event.type == Event::KeyPressed)
-                switch(event.key.code)
+            {
+                switch (event.key.code)
                 {
-                    case Keyboard::Left:  paddle.move(-speed, 0); break;
-                    case Keyboard::Right: paddle.move( speed, 0); break;
+                    case Keyboard::Left:
+                        if (paddle.getPosition().x - speed > 0)
+                            paddle.move(-speed, 0);
+                        break;
+                    case Keyboard::Right:
+                        if (paddle.getPosition().x + paddle.getSize().x + speed < window.getSize().x)
+                            paddle.move(speed, 0);
+                        break;
                 }
+            }
         }
+        
         window.clear(Color::White);
+        
         window.draw(paddle);
+        window.draw(ball);
+        
         window.display();
     }
     return EXIT_SUCCESS;
